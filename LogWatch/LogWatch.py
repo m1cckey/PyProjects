907de3d8edbf6d0e3e854cf9a3fc022be1d10b9d
Lenar <lenarg07@yandex.ru>
2026-05-05 01:28:49 +0300

    add detect_sql_injection

--- LogWatch/LogWatch.py
+++ LogWatch/LogWatch.py
@@ -2,7 +2,7 @@ import re
 
 def parser_log(path: str) -> list:
     result = []
-    regex = r'(?P<IP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (?P<auth_user>\w+|\-) (?P<remote_user>\w+|\-) (?P<date>\[\d{1,2}\/\w{3}\/\d{4}\:\d{2}\:\d{2}\:\d{2} [+-]\d+\]) (?P<method>\"[^\"]+\") (?P<status>\d{3}) (?P<size>\d+) (?P<http_refer>\"[^\"]+\") (?P<UA>\"[^\"]+\")'
+    regex = r'(?P<IP>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) (?P<auth_user>\w+|\-) (?P<remote_user>\w+|\-) (?P<date>\[\d{1,2}\/\w{3}\/\d{4}\:\d{2}\:\d{2}\:\d{2} [+-]\d+\]) (?P<full_method>\"[^\"]+\") (?P<status>\d{3}) (?P<size>\d+) (?P<http_refer>\"[^\"]+\") (?P<UA>\"[^\"]+\")'
     with open(path, 'r') as file:
         for line in file:
             match = re.search(regex, line)
@@ -11,7 +11,7 @@ def parser_log(path: str) -> list:
                 auth_user = match.group('auth_user')
                 remote_user = match.group('remote_user')
                 date = match.group('date')
-                method = match.group('method')
+                full_method = match.group('full_method')
                 status = match.group('status')
                 size = match.group('size')
                 http_refer = match.group('http_refer')
@@ -21,7 +21,7 @@ def parser_log(path: str) -> list:
                     "auth_user": auth_user,
                     "remote_user": remote_user,
                     "date": date,
-                    "method": method,
+                    "full_method": full_method,
                     "status": status,
                     "size": size,
                     "http_refer": http_refer,
@@ -31,4 +31,19 @@ def parser_log(path: str) -> list:
     return result
 
 
-print(parser_log('LogWatch/test_acces.log'))
+
+
+def detect_sql_injection(data: list):
+    regex = r'.+ (?P<pattern>(OR \'\d\'=\'\d)|(SELECT|DROP|DELETE|INSERT|UPDATE)|(\-\-)|(\/\*))'
+    result = []
+    for item  in data:
+        match = re.search(regex, item['full_method'], re.IGNORECASE)
+        if match:
+            triplet = (match.group('pattern'), item['full_method'], item['IP'], 'SQL Injection detected')
+            result.append(triplet)
+    return result
+
+
+sql = detect_sql_injection(parser_log('LogWatch/test_acces.log'))
+for pattern, full_method, ip, reason in sql:
+    print(f'pattern: {pattern}\nmethod: {full_method}\nIP: {ip}\nreason: {reason}') 
